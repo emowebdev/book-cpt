7d9da06eca0f759d4b17bc8ba8cdc1b0a58d7d9c
emowebdev <emoweb365@gmail.com>
2024-07-24 16:19:41 +0330

    Initialize plugin

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Book Post Type
+
+## Installation
+Once you've cloned the plugin in the plugin directory of your WordPress use the following command to install composer and all PHP dependencies
+
+```bash
+composer install
+```
---
+++ book-cpt.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package book-cpt
+ * Plugin Name: Book Post Type
+ * Plugin URI: https://github.com/emowebdev/book-cpt
+ * Description: A plugin which add book post type to your WordPress site
+ * Author: Emoweb
+ * Author URI: https://emoweb.dev
+ * Version: 1.0.0
+ * Requires at least: 5.8
+ * Tested up to: 6.2
+ * Text Domain: book_cpt
+ * Domain Path: /languages
+ **/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
