98a8b5555b023204ca2b18460019f9fe49b5f632
emowebdev <emoweb365@gmail.com>
2024-07-25 10:11:28 +0330

    Create Abstract_Post_Type_Register ckass

---
+++ includes/Post_Types/Abstract_Post_Type_Register.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Register post type
+ */
+
+ namespace BookCPT\Post_Types;
+
+ abstract class Abstract_Post_Type_Register
+ {
+    /**
+     * 
+     * @var string
+     * Post type key. Must not exceed 20 characters and may only contain lowercase alphanumeric characters, dashes, and underscores
+     */
+    protected string $post_type;
+
+    /**
+     * @var array
+     * Array or string of arguments for registering a post type
+     */
+    protected array $args;
+
+    abstract protected function init_properties();
+
+    protected function register()
+    {
+        $this->init_properties();
+        register_post_type($this->post_type, $this->args);
+    }
+ }
